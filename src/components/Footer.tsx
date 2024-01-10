3bc228ad46fcb78a5031998dc6d8979deb067381
brendenozie <brendenozie@gmail.com>
2024-01-10 16:28:53 +0300

    added the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,51 +5,46 @@ import LinkedIn from "../assets/linkedin.png";
 import Logo from '../assets/logo.png'
 const Footer = () => {
   return (
-    <div className="position: relative;">
-      <hr className="border: 1px solid var(--lightgray);"/>
-      <div className="padding: 1rem 2rem;
-                        display: flex;
-                        flex-direction: column;
-                        gap: 4rem;
-                        align-items: center;
-                        justify-content: center;
-                        height: 20rem;">
-        <div className="display: flex;
-                          gap: 4rem;">
-          <img src={Github.src}  className="width: 2rem;
-  height: 2rem;
-  cursor: pointer;" alt="" />
-          <img src={Instagram.src}  className="" alt="" />
-          <img src={LinkedIn.src}  className="" alt="" />
+    <div className="relative;">
+      <hr className="rounded-sm var(--lightgray);"/>
+      <div className="p-x-[1rem] p-y-[2rem]
+                        flex
+                        flex-col
+                        gap-[4rem]
+                        items-center
+                        justify-center
+                        h-[20rem]">
+        <div className="flex
+                          gap-[4rem]">
+          <img src={Github.src}  className="w-[2rem] h-[2rem] cursor-pointer" alt="" />
+          <img src={Instagram.src}  className="w-[2rem] h-[2rem] cursor-pointer" alt="" />
+          <img src={LinkedIn.src}  className="w-[2rem] h-[2rem] cursor-pointer" alt="" />
         </div>
-        <div className="width: 10rem;">
+        <div className="w-[10rem]">
             <img src={Logo.src} alt="" />
         </div>
       </div>
-      <div className="blur bottom: 0;
-                            right: 15%;
-                            width: 26rem;
-                            height: 12rem;
+      <div className="blur 
                             filter: blur(200px);
-                            background: red; 
-                            bottom: 0;
-                            right: 15%;
-                            width: 26rem;
-                            height: 12rem;
+                            bg-[#f43434] 
+                            bottom-[0]
+                            right-[15%]
+                            w-[26rem]
+                            h-[12rem]
                             filter: blur(200px);
-                            background: red;"></div>
-      <div className="blur bottom: 0;
-                right: 15%;
-                width: 26rem;
-                height: 12rem;
-                filter: blur(200px);
-                background: rgb(255, 115, 0);
-                bottom: 0;
-                left: 15%;
-                width: 26rem;
-                height: 12rem;
-                filter: blur(200px);
-                background: rgb(255, 115, 0);"></div>
+                            bg-red;"></div>
+      <div className="blur bottom-0
+                            right-[15%]
+                            w-[26rem]
+                            h-[12rem]
+                            filter: blur(200px)
+                            bg[rgb(255, 115, 0)]
+                            b-0
+                            left-[15%]
+                            w-[26rem]
+                            h-[12rem]
+                            filter: blur(200px)
+                            bg-[rgb(255, 115, 0)]"></div>
     </div>
   );
 };
